f82258389ffd346450af9c689ce8622316884e5c
s fargali <sfargali@gmail.com>
2020-01-02 15:38:25 -0500

    attempt to grab port directly through index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 const server = require("./server");
-const { port } = require("./config");
+//const { port } = require("./config");
+const port = process.env.PORT || 3000;
 
 server.listen(port,'0.0.0.0', () => {
   console.log(`
