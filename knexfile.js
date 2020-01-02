396f3d32fb0a878171363d0b0031377b22c46b16
s fargali <sfargali@gmail.com>
2020-01-02 14:16:07 -0500

    added migrations for users, tribes, projects, invs

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+const dbUrl = require("./config").dbUrl
+module.exports = {
+
+  development: {
+    client: "pg",
+    connection: dbUrl,
+    searchPath: ["knex", "public"],
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
