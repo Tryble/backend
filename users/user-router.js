e991a5db49445017ef3772a1aff747f7b5d5ce65
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2020-01-03 15:38:16 +0000

    file upload

--- users/user-router.js
+++ users/user-router.js
@@ -1,23 +1,31 @@
 const e = require("express");
 const User = require("./user-model");
 const Tribe = require("../tribes/tribe-model");
-const { generateToken } = require("../utils");
+const { generateToken, uploader } = require("../utils");
+const upload = require("../middlewares/multer");
 const { validateUser, validateLogin } = require("../middlewares/validations");
 
 const authRouter = e.Router();
 const userRouter = e.Router();
 
-authRouter.post("/register", validateUser, async (req, res, next) => {
-  try {
-    const [user] = await User.create(req.data);
-    const token = generateToken(user);
-    return res
-      .status(200)
-      .json({ message: `Welcome ${user.firstName}`, token });
-  } catch (error) {
-    return next(error);
+authRouter.post(
+  "/register",
+  upload.any(),
+  validateUser,
+  async (req, res, next) => {
+    try {
+      const [{ path }] = req.files;
+      const url = await uploader(path);
+      const [user] = await User.create({ ...req.data, imgUrl: url });
+      const token = generateToken(user);
+      return res
+        .status(200)
+        .json({ message: `Welcome ${user.firstName}`, token });
+    } catch (error) {
+      return next(error);
+    }
   }
-});
+);
 // POST /api/auth/login
 authRouter.post("/login", validateLogin);
 
