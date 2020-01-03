51c4f48ae2da71b406984dea19c116574eb6cce7
s fargali <sfargali@gmail.com>
2020-01-03 12:55:37 -0500

    seeds clear before reseeding, tribe uses cat image

--- data/seeds/tribe-seed.js
+++ data/seeds/tribe-seed.js
@@ -2,12 +2,14 @@ const faker = require("faker");
 
 const createFakeTribe = () => ({
   name: faker.lorem.words(),
-  imgUrl: faker.image.abstract()
+  imgUrl: faker.image.cats()
 })
 
 exports.seed = async function(knex) {
   const fakeTribes = [];
   const desiredFakeTribes = 50;
+
+  await knex('tribes').del()
   for (let i = 0; i < desiredFakeTribes; i++) {
     fakeTribes.push(createFakeTribe());
   }
