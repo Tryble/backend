22fad67b33898a14ad423ac7974167f871fd843a
s fargali <sfargali@gmail.com>
2020-01-03 11:08:41 -0500

    added faker, implemented project + tribe seeds

---
+++ data/seeds/project-seed.js
@@ -0,0 +1,18 @@
+const faker = require("faker");
+
+const createFakeProject = () => ({
+  name: faker.lorem.words(),
+  upvotes: faker.random.number(),
+  headline: faker.lorem.words(),
+  description: faker.lorem.words()
+})
+
+exports.seed = async function(knex) {
+  const fakeProjects = [];
+  const desiredFakeProjects = 50;
+  for (let i = 0; i < desiredFakeProjects; i++) {
+    fakeProjects.push(createFakeProject());
+  }
+  await knex("projects")
+    .insert(fakeProjects)
+};
